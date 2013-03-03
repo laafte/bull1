b3d6ebd0c5d27a3340af45e48e7582e6f02f8d86
teodoran <teodoran@stud.ntnu.no>
2013-03-03 19:55:53 +0100

    Started adding pages model and view

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Page(models.Model):
+	pageName = models.CharField(max_length = 1000)
+	descriptionText = models.TextField()
+
 # Create your models here.
--- pages/views.py
+++ pages/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from pages.models import Page
+
+def pages(request):
+	band_page_info_list = Page.objects.all()
+	t = loader('pages/home.html')
+	c = Context({
+		'band_page_info_list': band_page_info_list
+		})
+	return HttpResponse(t.render(c))
+
+
