91588f6c9ad41c4c913586859db4ed614948ad05
Johan Reitan <johan.reitan@gmail.com>
2013-05-10 19:43:50 +0200

    add page Category model

--- pages/models.py
+++ pages/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
-class Page(models.Model):
-	pageName = models.CharField(max_length = 1000)
-	descriptionText = models.TextField()
 
-# Create your models here.
+class Category(models.Model):
+    categoryName = models.CharField(max_length=1000)
+
+
+class Page(models.Model):
+    pageName = models.CharField(max_length=1000)
+    category = models.ForeignKey(Category)
+    descriptionText = models.TextField()
+
+
