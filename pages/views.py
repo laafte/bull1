8e3f54cc9d477ea6713991d98ad443b1e4f815ff
Johan Reitan <johan.reitan@gmail.com>
2013-05-10 21:12:49 +0200

    begin to fill template

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,9 @@
-from pages.models import Page
+from pages.models import Page, Category
 from django.shortcuts import render
 
 
 def pages(request):
     band_page_info_list = Page.objects.all()
-    return render(request, 'pages/index.html', {'band_page_info_list': band_page_info_list})
+    category_list = Category.objects.all()
+    return render(request, 'pages/index.html', {'band_page_info_list': band_page_info_list,
+                                                'category_list': category_list})
