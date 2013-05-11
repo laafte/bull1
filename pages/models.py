0e3b1fc0fccfb116fb49c3f4c878d8011817ec4c
Johan Reitan <johan.reitan@gmail.com>
2013-05-11 16:03:49 +0200

    nice instance names in admin

--- pages/models.py
+++ pages/models.py
@@ -4,10 +4,14 @@ from django.db import models
 class Category(models.Model):
     categoryName = models.CharField(max_length=1000)
 
+    def __unicode__(self):
+        return self.categoryName
+
 
 class Page(models.Model):
     pageName = models.CharField(max_length=1000)
     category = models.ForeignKey(Category)
     descriptionText = models.TextField()
 
-
+    def __unicode__(self):
+        return self.pageName
