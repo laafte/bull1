1169e88e08364c4e5c8f12939a185c68b37e2868
Johan Reitan <johan.reitan@gmail.com>
2013-05-13 19:33:14 +0200

    new views (empty yet)

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,20 @@
+from django.http.response import HttpResponse
 from pages.models import Page, Category
 from django.shortcuts import render
 
 
-def pages(request):
-    band_page_info_list = Page.objects.all()
-    category_list = Category.objects.all()
-    return render(request, 'pages/index.html', {'band_page_info_list': band_page_info_list,
-                                                'category_list': category_list})
+def index(request):
+    group_list = Page.objects.filter(category__category_name='Groups')
+    return render(request, 'pages/index.html', {'group_list': group_list})
+
+
+def info(request):
+    return render()
+
+
+def contact(request):
+    return render()
+
+
+def group(request, group):
+    return HttpResponse('ok')
