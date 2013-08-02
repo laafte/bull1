8f21ed2f8856fd6d18ff62456adbed69a6d96994
Johan Reitan <johan.reitan@gmail.com>
2013-08-02 16:50:52 +0200

    begin implementing info pages

--- pages/views.py
+++ pages/views.py
@@ -3,12 +3,20 @@ from django.shortcuts import render
 
 
 def index(request):
+    info_list = Page.objects.filter(category__exact='info')
     group_list = Page.objects.filter(category__exact='group').order_by('page_name')
-    return render(request, 'pages/index.html', {'group_list': group_list})
+    return render(request, 'pages/index.html', {'info_list': info_list,
+                                                'group_list': group_list})
 
 
-def info(request):
-    return render()
+def info(request, info):
+    info_page = Page.objects.filter(slug=info)[0]
+    info_list = Page.objects.filter(category__exact='info')
+    group_list = Page.objects.filter(category__exact='group').order_by('page_name')
+    return render(request, 'pages/info.html', {'info_name': info_page.page_name,
+                                               'description_text': info_page.description_text,
+                                               'info_list': info_list,
+                                               'group_list': group_list},)
 
 
 def contact(request):
