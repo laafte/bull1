760ff03163d96a8dae7dabd3d67ec4cfc9978039
Johan Reitan <johan.reitan@gmail.com>
2013-11-27 22:01:39 +0100

    more deployment adjustments

--- bull/wsgi.py
+++ bull/wsgi.py
@@ -1,5 +1,6 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bull.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
