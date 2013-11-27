dfe698674eda9d41856297907a7003fff88be380
Johan Reitan <johan.reitan@gmail.com>
2013-11-27 18:16:41 +0100

    update for django 1.6

--- settings.py
+++ settings.py
@@ -109,7 +109,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
 )
 
-ROOT_URLCONF = 'bull.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
