375e95138de51a546399e622259b21f9697e2bcc
Johan Reitan <johan.reitan@gmail.com>
2013-11-27 20:38:06 +0100

    cleaning according to deploydjango.com

---
+++ settings/dev.py
@@ -0,0 +1,4 @@
+# Load defaults in order to then add/override with dev-only settings
+from common import *
+
+DEBUG = True
---
+++ settings/prod.py
@@ -0,0 +1,4 @@
+# Load defaults in order to then add/override with production-only settings
+from common import *
+
+DEBUG = False
