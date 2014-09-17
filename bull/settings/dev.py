f8302ec027d23cf8e773003276a7724ddfd7d8f9
Johan Reitan <johan.reitan@gmail.com>
2014-09-17 14:40:56 +0200

    Fix python 3 issue

--- bull/settings/dev.py
+++ bull/settings/dev.py
@@ -1,4 +1,14 @@
 # Load defaults in order to then add/override with dev-only settings
-from common import *
+from .common import *
 
 DEBUG = True
+
+STATIC_ROOT = "/home/johan/PycharmProjects/bull/static"
+
+STATICFILES_DIRS = (
+    "/home/johan/PycharmProjects/bull/bull/staticfiles",
+)
+
+TEMPLATE_DIRS = (
+    "/home/johan/PycharmProjects/bull/bull/templates",
+)
